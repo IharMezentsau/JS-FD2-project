06b748bce5f87e7f673c6db462a0a4eaf1258cb0
StasGordynets <stasgordynets@gmail.com>
2018-11-03 13:44:06 +0300

    update errorpage
    update errorpage

--- src/404/js/script.js
+++ src/404/js/script.js
@@ -1,9 +1,64 @@
+document.getElementById('pageStatus').innerHTML = `<canvas id="star">
+
+    </canvas>
+   
+    <div class="hero">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8">
+                    <h1 id="nameError"></h1>
+                    <h2 id="errorText"></h2>
+                    <a href="#" class="btn-back">Вернутся на землю</a>
+                </div>
+                <div class="col-sm-4">
+                    <div class="roket">
+                        <img src="./img/rocket.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>`;
+
+
+function updatePage() {
+    let nameError = document.getElementById('nameError');
+    let errorText = document.getElementById('errorText');
+
+    switch (window.location.hash) {
+        case "#404": {
+            console.log("404");
+            nameError.innerText = "404";
+            errorText.innerText = "Похоже, вы вышли в открытый космос...";
+            break;
+        }
+        case "#401": {
+            console.log("401");
+            nameError.innerText = "401";
+            errorText.innerText = "Похоже, вы не авторизованы!";
+            break;
+        }
+        case "#500": {
+            console.log("500");
+            nameError.innerText = "500";
+            errorText.innerText = "Боже мой, у вас внутренняя ошибка сервера =(";
+            break;
+        }
+        default : {
+            console.log("400");
+            nameError.innerText = "400";
+            errorText.innerText = "Похоже, вам необходимо выкинуть комп...";
+            break;
+        }
+    }
+}
+
+
 var WIDTH = window.innerWidth,
     HEIGHT = window.innerHeight,
     MAX_PARTICLES = 100,
-    DRAW_INTERVAL = 60,
+    DRAW_INTERVAL = 1,
     container = document.querySelector('header'),
-    canvas = document.querySelector('#universe'),
+    canvas = document.querySelector('#star'),
     context = canvas.getContext('2d'),
     gradient = null,
     pixies = new Array();
@@ -16,10 +71,12 @@ function setDimensions(e) {
     canvas.width = WIDTH;
     canvas.height = HEIGHT;
 }
+
 setDimensions();
 window.addEventListener('resize', setDimensions);
+window.addEventListener("hashchange", updatePage);
 
-function Circle() {
+function Stars() {
     this.settings = {
         ttl: 800,
         xmax: 5,
@@ -86,7 +143,7 @@ function Circle() {
 }
 
 for (var i = 0; i < MAX_PARTICLES; i++) {
-    pixies.push(new Circle());
+    pixies.push(new Stars());
     pixies[i].reset();
 }
 
@@ -98,4 +155,5 @@ function draw() {
         pixies[i].draw();
     }
 }
+
 setInterval(draw, DRAW_INTERVAL);
