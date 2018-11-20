e87ba6b7f8c9da3b9a59cae2d30b13d6b12b29cb
maxim199921 <38768305+maxim199921@users.noreply.github.com>
2018-11-20 20:41:13 +0300

    Update NameView.js

--- src/app/NameView.js
+++ src/app/NameView.js
@@ -47,7 +47,7 @@ export class NameView {
         };
         this.namelist = document.getElementById("namelist");
         let counter = 0;
-        for(let key in arrName) {
+        for(let key of arrName) {
             // this.namelist.innerHTML += `<a class="mdl-navigation__link" id="${counter+1}" href="#"><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">person</i>${key}</a>`;
             this.list = document.createElement('a');
             this.list.setAttribute('class', 'mdl-navigation__link');
@@ -70,15 +70,15 @@ export class NameView {
         }
     }
 
-    siteBarChannel() {
-        this.changeChannel = document.getElementById("changeChannel");
-        this.changeChannel.style.marginTop = '50px';
-        this.buttonchangeChannel = document.createElement('a');
-        this.buttonchangeChannel.innerHTML += `Change channel`;
-        this.buttonchangeChannel.setAttribute('class', 'close');
-        this.buttonchangeChannel.setAttribute('href', '#');
-        this.changeChannel.appendChild(this.buttonchangeChannel);
-    }
+    // siteBarChannel() {
+    //     this.changeChannel = document.getElementById("changeChannel");
+    //     this.changeChannel.style.marginTop = '50px';
+    //     this.buttonchangeChannel = document.createElement('a');
+    //     this.buttonchangeChannel.innerHTML += `Change channel`;
+    //     this.buttonchangeChannel.setAttribute('class', 'close');
+    //     this.buttonchangeChannel.setAttribute('href', '#');
+    //     this.changeChannel.appendChild(this.buttonchangeChannel);
+    // }
 
     channelName(channel) {
         let inChannel = channel ? channel : 'general';
