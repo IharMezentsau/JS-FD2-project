141eef64b68cae8b72f3cb56b1281bbc4ec3568c
Andrei <andry-93@bk.ru>
2018-11-22 17:44:09 +0300

    Ошибки в промис

--- src/app/ChanelController.js
+++ src/app/ChanelController.js
@@ -5,16 +5,26 @@ export class ChanelController {
 		this.service = service;
 		this.view.render();
 		this.model.getStorage(this.model.stringChanel)
-			.then(response => {
-				this.model.chanelTemp = this.service.readReady(response, this.model.chanelTemp)
-				return this.model.getStorage(this.model.stringPerson)
-			})
-			.then(response => {
-				this.model.personTemp = this.service.readReady(response, this.model.personTemp)
-				this.view.chanelList(this.model.personTemp, this.model.user); //this.model.user
-				this.clickEvent (this.service, this.view, this.model, this.model.user);
-				this.clickCreate (this.service, this.view, this.model, this.model.user);
-			});
+			.then(
+				response => {
+					this.model.chanelTemp = this.service.readReady(response, this.model.chanelTemp)
+					return this.model.getStorage(this.model.stringPerson)
+				},
+				error => {
+					
+				}
+			)
+			.then(
+				response => {
+					this.model.personTemp = this.service.readReady(response, this.model.personTemp)
+					this.view.chanelList(this.model.personTemp, this.model.user); //this.model.user
+					this.clickEvent (this.service, this.view, this.model, this.model.user);
+					this.clickCreate (this.service, this.view, this.model, this.model.user);
+				},
+				error => {
+					
+				}
+			);
 	}
 	
 	clickEvent (service, view, model, user) {
@@ -25,23 +35,39 @@ export class ChanelController {
 				service.delChanel(evt.target, model.chanelTemp, model.personTemp, user);
 				view.disableButton();
 				model.storeInfo(model.stringChanel)
-				.then(response => {
-					model.lockGetReady(response, model.stringChanel, model.chanelTemp)
-					return model.storeInfo(model.stringPerson);
+				.then(
+					response => {
+						model.lockGetReady(response, model.stringChanel, model.chanelTemp)
+						return model.storeInfo(model.stringPerson);
+					},
+				error => {
+					console.error(error);
 				})
-				.then(response => {
-					model.lockGetReady(response, model.stringPerson, model.personTemp)
-					return model.getStorage(model.stringChanel);
-				})
-				.then(response => {
-					model.chanelTemp = service.readReady(response, model.chanelTemp);
-					return model.getStorage(model.stringPerson)
-				})
-				.then(response => {
-					model.personTemp = service.readReady(response, model.personTemp);
-					view.disableButton();
-					view.chanelList(model.personTemp, user);
-				});
+				.then(
+					response => {
+						model.lockGetReady(response, model.stringPerson, model.personTemp)
+						return model.getStorage(model.stringChanel);
+					},
+					error => {
+						console.error(error);
+					})
+				.then(
+					response => {
+						model.chanelTemp = service.readReady(response, model.chanelTemp);
+						return model.getStorage(model.stringPerson)
+					},
+					error => {
+						console.error(error);
+					})
+				.then(
+					response => {
+						model.personTemp = service.readReady(response, model.personTemp);
+						view.disableButton();
+						view.chanelList(model.personTemp, user);
+					},
+					error => {
+						console.error(error);
+					})
 			} else if (evt.target.classList.value.indexOf('channel-link') != -1) { // переход на канал
 				service.checkChannel(evt.target);
 			}
@@ -55,23 +81,39 @@ export class ChanelController {
 			service.cChanel(model.chanelTemp, model.personTemp, user);
 			view.disableButton();
 			model.storeInfo(model.stringChanel)
-				.then(response => {
-					model.lockGetReady(response, model.stringChanel, model.chanelTemp)
-					return model.storeInfo(model.stringPerson);
-				})
-				.then(response => {
-					model.lockGetReady(response, model.stringPerson, model.personTemp)
-					return model.getStorage(model.stringChanel);
-				})
-				.then(response => {
-					model.chanelTemp = service.readReady(response, model.chanelTemp);
-					return model.getStorage(model.stringPerson)
-				})
-				.then(response => {
-					model.personTemp = service.readReady(response, model.personTemp);
-					view.disableButton();
-					view.chanelList(model.personTemp, user);
-				});
+				.then(
+						response => {
+						model.lockGetReady(response, model.stringChanel, model.chanelTemp)
+						return model.storeInfo(model.stringPerson);
+					},
+					error => {
+						console.error(error);
+					})
+				.then(
+					response => {
+						model.lockGetReady(response, model.stringPerson, model.personTemp)
+						return model.getStorage(model.stringChanel);
+					},
+					error => {
+						console.error(error);
+					})
+				.then(
+					response => {
+						model.chanelTemp = service.readReady(response, model.chanelTemp);
+						return model.getStorage(model.stringPerson)
+					},
+					error => {
+						console.error(error);
+					})
+				.then(
+					response => {
+						model.personTemp = service.readReady(response, model.personTemp);
+						view.disableButton();
+						view.chanelList(model.personTemp, user);
+					},
+					error => {
+						console.error(error);
+					});
 		})
 	}
 }
