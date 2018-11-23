e2aca56959a30c64c05b3b58200d8d858341a56d
Ihar Mezentsau <iharmezentsau@gmail.com>
2018-11-24 00:11:21 +0300

    delete btnError fix errorAudio

--- src/app/ErrorView.js
+++ src/app/ErrorView.js
@@ -1,3 +1,5 @@
+import {PubSubService} from "./PubSubService";
+
 export class ErrorView {
     constructor(root) {
         console.log('View constructor');
@@ -15,14 +17,14 @@ export class ErrorView {
     }
 
     addAudioStream() {
-        var audio = new Audio('http://stream.hoster.by:8081/pilotfm/pilot/icecast.audio');
-        audio.play();
+        this.audio = new Audio('http://stream.hoster.by:8081/pilotfm/pilot/icecast.audio');
+        this.audio.play();
     }
 
     add_HTML_Data() {
 
         this.addAudioStream();
-
+        new PubSubService().sub('stopErrorAudio', () => this.audio.pause());
         this.root.innerHTML = `<header class="headerStas"><canvas id='star'> </canvas>
         <div id="hero" class="hero">
         <div class="container">
@@ -30,7 +32,6 @@ export class ErrorView {
                 <div>
                     <h1 id="nameError"> </h1>
                     <h2 id="errorText"> </h2>
-                    <a href="#auth" onclick="hrefAction()" class="btn-back">Вернутся на землю</a>
                 </div>
                 <div>
                     <div class="image">
@@ -45,11 +46,6 @@ export class ErrorView {
         this.container = document.querySelector('header');
         this.canvas = document.querySelector('#star');
         this.context = this.canvas.getContext('2d');
-    }
-
-    hrefAction(){
-        console.log('location change');
-        window.location='#auth';
     }
 
     initStars() {
